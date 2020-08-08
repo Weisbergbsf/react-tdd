248d73ea28d35a49c130f2bb0c0fab344082f215
Weisberg Barros <weisbergbsf@gmail.com>
2020-08-08 20:23:39 -0300

    feat: ensure RemoteAddAccount throws EmailInUseError if HttpPostClient returns 403

--- src/domain/test/mock-add-account.ts
+++ src/domain/test/mock-add-account.ts
@@ -1,7 +1,7 @@
 import { AddAccountParams } from '@/domain/usecases'
 import faker from 'faker'
 
-export const mockAccountParams = (): AddAccountParams => {
+export const mockAddAccountParams = (): AddAccountParams => {
   const password = faker.internet.password()
   return {
     name: faker.name.findName(),
