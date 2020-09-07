4caf6cd53da1cab7fd92c882073167e8478013bc
Weisberg Barros Siqueira Fontes <weisberg.fontes@softplan.com.br>
2020-09-07 20:11:31 -0300

    refactor: organize integration tests with fixtures

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -1,32 +1,36 @@
-import * as Helper from '../support/helpers'
-import * as Http from '../support/survey-list-mocks'
-import faker from 'faker'
+import * as Helper from '../utils/helpers'
+import * as Http from '../utils/http-mocks'
+
+const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
+const mockAccessDenieddError = (): void => Http.mockForbiddenError(/surveys/, 'GET')
 
 describe('SurveyList ', () => {
   beforeEach(() => {
-    Helper.setLocalStorageItem('account', { accessToken: faker.random.uuid(), name: faker.name.findName() })
+    cy.fixture('account').then(account => {
+      Helper.setLocalStorageItem('account', account)
+    })
   })
   it('Should present error on UnexpectedError', () => {
-    Http.mockUnexpectedError()
+    mockUnexpectedError()
     cy.visit('')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
   })
 
   it('Should logout on AccessDeniedError', () => {
-    Http.mockAccessDenieddError()
+    mockAccessDenieddError()
     cy.visit('')
     Helper.testUrl('/login')
   })
 
   it('Should present correct username', () => {
-    Http.mockUnexpectedError()
+    mockUnexpectedError()
     cy.visit('')
     const { name } = Helper.getLocalStorageItem('account')
     cy.getByTestId('username').should('contain.text', name)
   })
 
   it('Should logout on logout link click', () => {
-    Http.mockUnexpectedError()
+    mockUnexpectedError()
     cy.visit('')
     cy.getByTestId('logout').click()
     Helper.testUrl('/login')
--- src/main/test/cypress/support/survey-list-mocks.ts
+++
@@ -1,4 +0,0 @@
-import * as Http from './http-mocks'
-
-export const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'GET')
-export const mockAccessDenieddError = (): void => Http.mockForbiddenError(/surveys/, 'GET')
