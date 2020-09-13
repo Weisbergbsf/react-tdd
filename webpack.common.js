a3c4a32981fba6b4bf627c17adbf0fb1c180465a
Weisberg Barros Siqueira Fontes <weisberg.fontes@softplan.com.br>
2020-09-13 16:53:27 -0300

    feat: add node server to serve static files

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,6 @@ module.exports = {
   entry: './src/main/index.tsx',
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/dist',
     filename: 'main-bundle-[hash].js'
   },
   resolve: {
