8aabc768c8c82a0706e9ca9511a157bdf25fabec
Weisberg Barros Siqueira Fontes <weisberg.fontes@softplan.com.br>
2020-09-13 19:04:51 -0300

    chore: config travis ci to use cypress

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -75,6 +75,7 @@ describe('Login ', () => {
     mockSuccess()
     populateFields()
     cy.getByTestId('submit').dblclick()
+    cy.wait('@request')
     Helper.testHttpCallsCount(1) // para contar quantas vezes a requisição foi executada
   })
 
